4891f58baf13240eb22145c5047107480bf5bb14
Anthony Nguyen <anthony@couchdb.anthony.com>
2016-03-30 10:26:34 -0400

    Swich to hello anthony

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
  
 app.get('/', function (req, res) {
-  res.send('hello yuna');
+  res.send('hello anthony');
 });
  
 app.listen(process.env.PORT || 5000);
