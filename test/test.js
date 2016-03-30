23db29b6a3ebefbf4a3a7280a4854e17462ff363
Anthony Nguyen <anthony@couchdb.anthony.com>
2016-03-30 13:43:42 -0400

    Update test to hello agis

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello anthony', done);
+    request(app).get('/').expect('hello agis', done);
   });
 });
